585c099a420137b89ca08acad2655b07c98c537a
joneshf <jones3.hardy@gmail.com>
2013-12-15 10:44:41 -0800

    Added annotations to try and figure out what went wrong.

--- JezzBall.elm
+++ JezzBall.elm
@@ -32,9 +32,11 @@ import Window
 -- Keyboard
 data KeyInput = KeyInput Bool Bool Bool Bool
 
+defaultKeyInput : KeyInput
 defaultKeyInput = KeyInput False False False False
 
 -- We care about the space, N, P, and H keys
+keyInput : Signal KeyInput
 keyInput = lift4 KeyInput Keyboard.space
                           (Keyboard.isDown 78) -- N
                           (Keyboard.isDown 80) -- P
@@ -45,18 +47,22 @@ keyInput = lift4 KeyInput Keyboard.space
 -- We need to know when the user clicks.
 data MouseInput = MouseInput Bool
 
+defaultMouseInput : MouseInput
 defaultMouseInput = MouseInput False
 
+mouseInput : Signal MouseInput
 mouseInput = lift MouseInput Mouse.isClicked
 
 -- Clock
 
 -- Set the target frame rate.
+delta : Signal Time
 delta = lift inSeconds (fps 50)
 
 -- Combine all the inputs.
 data Input = Input Float KeyInput MouseInput
 
+input : Signal Input
 input = sampleOn delta (lift3 Input delta keyInput mouseInput)
 
 -- Game inputs
@@ -72,11 +78,19 @@ data State  = Level Int | BetweenLevels
 data GameState = GameState State Score Cover Balls Cursor
 
 -- Static game stuff.
+gameWidth : number
 gameWidth = 640
+
+gameHeight : number
 gameHeight = 480
+
+halfWidth : number
 halfWidth = gameWidth / 2
+
+halfHeight : number
 halfHeight = gameHeight / 2
 
+defaultGame : GameState
 defaultGame = GameState BetweenLevels
                         (Score 0)
                         (Cover 0)
@@ -85,15 +99,27 @@ defaultGame = GameState BetweenLevels
 
 -- Updates
 
--- Cursor movement.
+-- Cursor helpers.
+toFloat2 : (Int, Int) -> (Float, Float)
+toFloat2 (x, y) = (toFloat x, toFloat y)
+
+newPos : (Int, Int) -> (Int, Int) -> (Float, Float)
+newPos (x, y) (w, h) = (toFloat x - (toFloat w /  2), toFloat h / 2 - toFloat y)
 
 -- Ball helpers
+makePos : number -> number
 makePos n = if n > 0 then n else (-n)
+
+makeNeg : number -> number
 makeNeg n = if n < 0 then n else (-n)
+
+within : Float -> Float -> Float -> Bool
 within ep n x = n - ep < x && x < n + ep
 
+stepVel : Float -> Bool -> Bool -> Float
 stepVel v l u = if l then makePos v else if u then makeNeg v else v
 
+stepBall : Float -> Ball -> Ball
 stepBall d (Ball (x, y) (vx, vy)) =
     let vx' = stepVel vx (x < 7 - halfWidth) (x > halfWidth - 7)
         vy' = stepVel vy (y < 7 - halfHeight) (y > halfHeight - 7)
@@ -101,6 +127,7 @@ stepBall d (Ball (x, y) (vx, vy)) =
         y' = y + vy' * d
     in (Ball (x', y') (vx', vy'))
 
+stepGame : Input -> GameState -> GameState
 stepGame (Input d (KeyInput space n p h) (MouseInput clk))
          (GameState state score cov (Balls balls) cursor) =
     let balls' = case state of
@@ -110,29 +137,37 @@ stepGame (Input d (KeyInput space n p h) (MouseInput clk))
             -- Should update level if everything is good.
             (Level n)     -> state
             BetweenLevels -> if n then Level 1 else state
-    in GameState state' score cov (Balls balls') cursor
+        pos' = lift2 newPos Mouse.position Window.dimensions
+        cursor' = Cursor pos'
+    in GameState state' score cov (Balls balls') cursor'
 
+gameState : Signal GameState
 gameState = foldp stepGame defaultGame input
 
 -- View
 
-scoreBoard w inPlay = "Press N to begin"
+bg : Color
+bg = rgb 60 100 60
 
+textGreen : Color
 textGreen = rgb 160 200 160
 
+txt : (Text -> Text) -> String -> Element
 txt f = text . f . monospace . Text.color textGreen . toText
 
+msg : String
 msg = "Press N to begin"
 
+make : (Float, Float) -> Shape -> Form
 make (x, y) shape = shape |> filled white
                           |> move (x, y)
 
+makeBall : Ball -> Form
 makeBall (Ball pos vel) = circle 7 |> filled white
                           |> move pos
 
-bg = rgb 60 100 60
-
 -- Draw everything.
+display : (Int, Int) -> GameState -> Element
 display (w, h) (GameState state score cover (Balls balls) (Cursor c)) =
     container w h middle <| collage gameWidth gameHeight <|
         [ rect gameWidth gameHeight |> filled bg
@@ -140,4 +175,5 @@ display (w, h) (GameState state score cover (Balls balls) (Cursor c)) =
         , toForm <| if state == BetweenLevels then txt id msg else spacer 1 1
         ] ++ map makeBall balls
 
+main : Signal Element
 main = lift2 display Window.dimensions gameState
